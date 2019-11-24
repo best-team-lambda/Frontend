6a4cc688de4761a03196baedf4030998623949b3
Jordan Miller <jordan.j.miller@live.com>
2019-11-24 12:26:43 -0800

    progress on courseBuilder, dropdowns working correctly

--- src/components/CourseBuilder.js
+++ src/components/CourseBuilder.js
@@ -1,206 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
+import { selectCourse, selectUnit, selectWeek, selectDay } from '../actions/CourseBuilderActions.js';
 
 
 
 
 
 function CourseBuilder(props) {
-    const [courses, setCourses] = useState('');
-    const [units, setUnits] = useState('');
-    const [weeks, setWeeks] = useState('');
-    const [days, setDays] = useState('');
 
-    const [selectingCourse, setSelectingCourse] = useState(true);
-    const [selectingUnit, setSelectingUnit] = useState(false);
-    const [selectingWeek, setSelectingWeek] = useState(false);
-    const [selectingDay, setSelectingDay] = useState(false);
-
-    const [selectedCourse, setSelectedCourse] = useState('');
-    const [selectedUnit, setSelectedUnit] = useState('');
-    const [selectedWeek, setSelectedWeek] = useState('');
-    const [selectedDay, setSelectedDay] = useState('');
-
-
-
-    useEffect(() => {
-        setCourses(
-            [{
-                id: 100564, 
-                name: 'FSW', 
-                units: 
-                    [
-                        {
-                            id: 1231231,
-                            name: 'abc',
-                            weeks: 
-                            [
-                                {
-                                    id: 122311, 
-                                    name: 'abc', 
-                                    Days: [
-                                        {
-                                            id: 221,
-                                            name: 'monday'
-                                        },
-                                        {
-                                            id: 222,
-                                            name: 'tuesday'
-                                        },
-                                        {
-                                            id: 223,
-                                            name: 'wednesday'
-                                        },
-                                        {
-                                            id: 224,
-                                            name: 'thursday'
-                                        },
-                                    ] 
-                                },
-                                {
-                                    id: 231231231, 
-                                    name: 'week 3', 
-                                    Days: [
-                                        {
-                                            id: 331,
-                                            name: 'monday'
-                                        },
-                                        {
-                                            id: 332,
-                                            name: 'tuesday'
-                                        },
-                                        {
-                                            id: 333,
-                                            name: 'wednesday'
-                                        },
-                                        {
-                                            id: 334,
-                                            name: 'thursday'
-                                        },
-                                    ] 
-                                },
-                            ]
-                        },
-                        {
-                            id: 323213212,
-                            name: 'dacf',
-                            weeks: 
-                            [
-                                
-                                {
-                                    id: 1223311, 
-                                    name: 'abac', 
-                                    Days: [
-                                        {
-                                            id: 2321,
-                                            name: 'monday'
-                                        },
-                                        {
-                                            id: 2232,
-                                            name: 'tuesday'
-                                        },
-                                        {
-                                            id: 2233,
-                                            name: 'wednesday'
-                                        },
-                                        {
-                                            id: 2234,
-                                            name: 'thursday'
-                                        },
-                                    ] 
-                                },
-                                {
-                                    id: 23133231231, 
-                                    name: 'week 3', 
-                                    Days: [
-                                        {
-                                            id: 3331,
-                                            name: 'monday'
-                                        },
-                                        {
-                                            id: 3332,
-                                            name: 'tuesday'
-                                        },
-                                        {
-                                            id: 3333,
-                                            name: 'wednesday'
-                                        },
-                                        {
-                                            id: 3334,
-                                            name: 'thursday'
-                                        },
-                                    ] 
-                                },
-                            ]
-                        },
-                    ]
-            }]);
-    }, [])
-
-    const handleChange = () => {
-
-    }
-    const handleSelect = (e) => {
+    const handleChange = (e) => {
         e.persist();
         console.log(e);
         console.log(e.target);
         console.log('e.target.name :', e.target.name);
         console.log(e.target.value);
-        if (!selectedCourse && e.target.name === 'selectCourse' && e.target.value != 'AddCourse')
+        if (e.target.value === 'AddCourse')
         {
-            const newVar = 
-                courses.filter(course => {
-                console.log(course.id, e.target.value)
-                return course.id == e.target.value;
-                })
-                console.log('course: ', newVar[0]);
-                console.log('course.units: ', newVar[0].units);
-            setSelectedCourse({...newVar[0]})
-            setUnits([...newVar[0].units]);
+            
+            // setSelectedCourse({...newVar[0]})
+            // setUnits([...newVar[0].units]);
             return null;
         }
-        else {
-            setSelectedCourse('');
+        else if (e.target.value && e.target.value != 'default' && !props.courseSelected){
+            console.log('o ya')
+            props.selectCourse(e.target.value);
         }
-        if (selectedCourse && e.target.name === 'selectUnit' && e.target.value != 'AddUnit')
-        {
-            console.log(e.target.name)
-            console.log('units: ', units);
-            const newVar = 
-                units.filter(unit => {
-                console.log(unit.id, e.target.value)
-                return unit.id == e.target.value;
-                })
-                console.log('unit: ', newVar);
-            setSelectedUnit({...newVar[0]})
-            setWeeks([...newVar[0].weeks]);
-            return null;
+        else if (e.target.value && e.target.value != 'default' && !props.unitSelected){
+            console.log('o yaaaa')
+            props.selectUnit(e.target.value);
         }
-        else {
-            setSelectedUnit('');
-        }
-        console.log(e.target.name);
-        if (selectedUnit && e.target.name === 'selectWeek' && e.target.value != 'AddUnit')
-        {
-            console.log(e.target.name)
-            const newVar = 
-                units.filter(unit => {
-                console.log(unit.id, e.target.value)
-                return unit.id == e.target.value;
-                })
-                console.log('unit: ', newVar);
-            setSelectedWeek({...newVar[0]})
-            setWeeks([...newVar[0].weeks]);
-            return null;
-        }
-        else {
-            setSelectedWeek('');
+        else if (e.target.value && e.target.value != 'default' && !props.weekSelected){
+            console.log('o yaaaa week')
+            props.selectWeek(e.target.value);
         }
     }
     const addThing = () => {
 
     }
 
+    console.log('Props Course',props.courses)
+    console.log('Props courseSelected',props.courseSelected)
+    console.log('Props Units',props.units)
+    console.log('Props unitSelected',props.unitSelected)
+    console.log('Props Weeks',props.weeks)
+    console.log('Props weekSelected',props.weekSelected)
+    console.log('Props Days',props.days)
+    console.log('Props daySelected',props.daySelected)
     return (            
         <div className='filterToolsDiv'>
         {(()=>{ //immediately invoked function to allow javascript inside JSX. syntax: {(()=>{})()}
@@ -208,16 +53,37 @@ function CourseBuilder(props) {
                 <>
                     <p>CourseBuilder:</p>
                     <div className="select">
-                        <select id="selectCoufghrse" onChange={handleSelect} name="selectCourse">
-                                <option default>Select Course</option>
-                            {courses && courses.map(course => {
+                        <select id="selectCoufghrse" onChange={handleChange} name="selectCourse">
+                                <option default value='default'>Select Course</option>
+                            {props.courses && props.courses.map(course => {
                                 return (
                                     <option key={course.id} value={course.id}>{course.name}</option>
                                 );
                             })}
                             <option value="AddCourse">Add Course</option>
                         </select>
-                        {selectedCourse != '' && selectedCourse != 'AddCourse' && 
+
+                        {props.courseSelected && 
+                        <select onChange={handleChange} name="selectUnit">
+                            <option default value='default'>Select Unit</option>
+                            {props.units && props.units.map(unit=>{
+                            return <option key={unit.id} value={unit.id}>{unit.name}</option>
+                            })}
+                            <option value="AddUnit">Add Unit</option>
+                        </select>
+                        }
+                       
+                        {props.unitSelected && 
+                        <select onChange={handleChange} name="selectWeek">
+                            <option default value='default'>Select Week</option>
+                            {props.weeks && props.weeks.map(week=>{
+                            return <option key={week.id} value={week.id}>{week.name}</option>
+                            })}
+                            <option value="AddWeek">Add Week</option>
+                        </select>
+                        }
+
+                        {/* {selectedCourse != '' && selectedCourse != 'AddCourse' && 
                         <>
                             <select id="selechfghtedUnit" onChange={handleSelect} name="selectUnit">
                                 {units && units.map(unit => {
@@ -229,50 +95,14 @@ function CourseBuilder(props) {
                                 <option value="AddUnit">Add Unit</option>
                             </select>
                             </>
-                        } 
-                        {selectedUnit != '' && selectedUnit != 'AddUnit' &&
-                        <>
-                            <select id="selectegfhdWeek" onChange={handleSelect} name="selectWeek">
-                            {weeks && weeks.map(week => {
-                                console.log('weekselect', selectedUnit)
-                                console.log('weekselect')
-                                return (
-                                    <option key={week.id} value={week.id}>{week.name}</option>
-                                );
-                            })}
-                            <option value="AddWeek">Add Week</option>
-                            </select>
-                            </>
-                        } 
-
-                        {selectedWeek != '' && selectedWeek != 'AddWeek' &&
-                            <select id="selectedUnit" onChange={handleSelect} name="selectDay">
-                            <option default>Select Day</option>
-                            {days && days.map(day => {
-                                return (
-                                    <option key={day.id} value={day.id}>{day.name}</option>
-                                );
-                            })}
-                            <option value="AddDay">Add Day</option>
-                            </select>
-                        }
-
-
-
-
-
-
-
-
-
-
+                        }  */}
 
 
                     </div>
                     <input  className='searchBox' name="searchTerm" type="text" onChange={handleChange} placeholder="Filter" />
                     <br />
-                    <button className="button" onClick={addThing}>Add</button>
-                    <button className="button" onClick={()=>{console.log(selectedCourse);}}>log</button>
+                    {/* <button className="button" onClick={addThing}>Add</button>
+                    <button className="button" onClick={()=>{console.log(selectedCourse);}}>log</button> */}
                     <br />
                 </>
             );
@@ -288,7 +118,18 @@ const mapStateToProps = state => {
         unitSelected: state.unitSelected,
         weekSelected: state.weekSelected,
         daySelected: state.daySelected,
+        
+        selectedCourse: state.selectedCourse,
+        selectedUnit: state.selectedUnit,
+        selectedWeek: state.selectedWeek,
+        selectedDay: state.selectingDay,
+
+        courses: state.courses,
+        units: state.units,
+        weeks: state.weeks,
+        days: state.days,
+        day: state.day,
     }
   }
 
-export default connect(mapStateToProps, {  })(CourseBuilder)
+export default connect(mapStateToProps, { selectCourse, selectUnit, selectWeek, selectDay })(CourseBuilder)
--- src/reducers/CourseBuilderReducer.js
+++ src/reducers/CourseBuilderReducer.js
@@ -1,24 +1,199 @@
-import { FETCH_POKEMON_START, } from '../actions/CourseBuilderActions.js';
+import { SELECT_COURSE, SELECT_UNIT, SELECT_WEEK, SELECT_DAY } from '../actions/CourseBuilderActions.js';
 
 const initialState = {
     courseSelected: false,
     unitSelected: false,
     weekSelected: false,
     daySelected: false,
+    
+    selectedCourse: '',
+    selectedUnit: '',
+    selectedWeek: '',
+    selectedDay: '',
+
+    courses: [
+        {
+        id: 100564, 
+        name: 'FSW', 
+        units: 
+            [
+                {
+                    id: 1231231,
+                    name: 'abc',
+                    weeks: 
+                    [
+                        {
+                            id: 122311, 
+                            name: 'abc', 
+                            Days: [
+                                {
+                                    id: 221,
+                                    name: 'monday'
+                                },
+                                {
+                                    id: 222,
+                                    name: 'tuesday'
+                                },
+                                {
+                                    id: 223,
+                                    name: 'wednesday'
+                                },
+                                {
+                                    id: 224,
+                                    name: 'thursday'
+                                },
+                            ] 
+                        },
+                        {
+                            id: 231231231, 
+                            name: 'week 3', 
+                            Days: [
+                                {
+                                    id: 331,
+                                    name: 'monday'
+                                },
+                                {
+                                    id: 332,
+                                    name: 'tuesday'
+                                },
+                                {
+                                    id: 333,
+                                    name: 'wednesday'
+                                },
+                                {
+                                    id: 334,
+                                    name: 'thursday'
+                                },
+                            ] 
+                        },
+                    ]
+                },
+                {
+                    id: 323213212,
+                    name: 'dacf',
+                    weeks: 
+                    [
+                        
+                        {
+                            id: 1223311, 
+                            name: 'abac', 
+                            Days: [
+                                {
+                                    id: 2321,
+                                    name: 'monday'
+                                },
+                                {
+                                    id: 2232,
+                                    name: 'tuesday'
+                                },
+                                {
+                                    id: 2233,
+                                    name: 'wednesday'
+                                },
+                                {
+                                    id: 2234,
+                                    name: 'thursday'
+                                },
+                            ] 
+                        },
+                        {
+                            id: 23133231231, 
+                            name: 'week 3', 
+                            Days: [
+                                {
+                                    id: 3331,
+                                    name: 'monday'
+                                },
+                                {
+                                    id: 3332,
+                                    name: 'tuesday'
+                                },
+                                {
+                                    id: 3333,
+                                    name: 'wednesday'
+                                },
+                                {
+                                    id: 3334,
+                                    name: 'thursday'
+                                },
+                            ] 
+                        },
+                    ]
+                },
+            ]
+        }
+    ],
+    units: '',
+    weeks: '',
+    days: '',
+    day: '',
   };
 
 
 export const CourseBuilderReducer = (state = initialState, action) => {
-    //console.log('Reducer initialState: ', initialState);
-    //console.log('reducer firing: ', action);
+    console.log('Reducer initialState: ', initialState);
+    console.log('reducer firing: ', action);
     switch(action.type) {
-        case FETCH_POKEMON_START:
-            //console.log('FETCH_POKEMON_START', state);
+        case SELECT_COURSE:
+            console.log('SELECT_COURSE FIRING', state);
+            console.log('Payload: ', action.payload);
+            let pickedCourse = state.courses.find(course => {
+                console.log(course.id);
+                console.log(action.payload);
+                return course.id == action.payload;
+            })
+            console.log('pickedCourse', pickedCourse);
             return {
                 ...state,
-                isFetching: true,
-                error: ''
+                courseSelected: true,
+                selectedCourse: action.payload,
+                units: [...pickedCourse.units]
             };
+            case SELECT_UNIT:
+                console.log('SELECT_UNIT FIRING', state);
+                console.log('Payload: ', action.payload);
+                let pickedUnit = state.units.find(unit => {
+                    console.log(unit.id);
+                    console.log(action.payload);
+                    return unit.id == action.payload;
+                })
+                console.log('pickedUnit', pickedUnit);
+                return {
+                    ...state,
+                    unitSelected: true,
+                    selectedUnit: action.payload,
+                    weeks: [...pickedUnit.weeks]
+                };
+            case SELECT_WEEK:
+                console.log('SELECT_WEEK FIRING', state);
+                console.log('Payload: ', action.payload);
+                let pickedWeek = state.weeks.find(week => {
+                    console.log(week.id);
+                    console.log(action.payload);
+                    return week.id == action.payload;
+                })
+                console.log('pickedWeek', pickedWeek);
+                return {
+                    ...state,
+                    weekSelected: true,
+                    selectedWeek: action.payload,
+                    days: [...pickedWeek.Days]
+                };
+            case SELECT_DAY:
+                console.log('SELECT_DAY FIRING', state);
+                console.log('Payload: ', action.payload);
+                let pickedDay = state.days.find(day => {
+                    console.log(day.id);
+                    console.log(action.payload);
+                    return day.id == action.payload;
+                })
+                console.log('pickedDay', pickedDay);
+                return {
+                    ...state,
+                    DaySelected: true,
+                    selectedDay: action.payload,
+                    day: pickedDay,
+                };
         default: //console.log('REDUCER DEFAULT'); 
         return state;
   }
